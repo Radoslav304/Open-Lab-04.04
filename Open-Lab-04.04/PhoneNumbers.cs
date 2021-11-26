bfb3bc97be3e2aa0b5bbea7f2f37e6581df4ea47
Radoslav304 <radoslavcabadaj304@gmail.com>
2021-11-26 08:10:16 +0100

    A function that formats an integer array into a phone number.

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -6,7 +6,22 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+            string d = "";
+            string e = "";
+            string f = "";
+            for (int a = 0; a < 3; a++)
+            {
+                d = d + numbers[a];
+            }
+            for (int b = 3; b < 6; b++)
+            {
+                e = e + numbers[b];
+            }
+            for (int c = 6; c < 10; c++)
+            {
+                f = f + numbers[c];
+            }
+            return ($"({d}) {e}-{f}");
         }
     }
 }
